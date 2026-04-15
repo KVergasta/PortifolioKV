52cff23e178783fb496630f90777ac6e4339bc7f
Kauane <kauane.rodrigues@emabsa.ba.gov.br>
2026-04-15 11:59:36 -0300

    adicionando conteudo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [];
 
